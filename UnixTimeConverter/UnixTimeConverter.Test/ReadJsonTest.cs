84605f4ea6fb106a3f1bba9428495ad0f18f8f63
Egor Grishechko <egorgrishechko@gmail.com>
2017-08-07 23:11:15 +0300

    Tests, bagfixing
    Added some unit-tests
    Replace (long) cast with Convert.ToInt64

---
+++ UnixTimeConverter/UnixTimeConverter.Test/ReadJsonTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace UnixTimeConverter.Test
+{
+    public class ReadJsonTest
+    {
+        [Fact]
+        public void WriteJson_InvalidTokenType_ExceptionThrown()
+        {
+            //Arrange
+            var unixTimeConverter = new UnixTimeConverter();
+            var jsonReaderMock = new Mock<JsonReader>();
+            jsonReaderMock.SetupGet(x => x.TokenType).Returns(JsonToken.Boolean);
+
+            //Act
+            var ex = Assert.Throws<ArgumentException>(() => unixTimeConverter.ReadJson(jsonReaderMock.Object, null, false, null));
+
+            //Assert
+            Assert.Equal("Unexpected token parsing date. Integer or String was expected, got Boolean", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(JsonToken.Integer, 1355314332)]
+        [InlineData(JsonToken.String, 1355314332)]
+        public void WriteJson_ValidPositiveInput_Success(JsonToken jsonTokenType, object value)
+        {
+            //Arrange
+            var unixTimeConverter = new UnixTimeConverter();
+            var jsonReaderMock = new Mock<JsonReader>();
+            jsonReaderMock.SetupGet(x => x.TokenType).Returns(jsonTokenType);
+            jsonReaderMock.SetupGet(x => x.Value).Returns(value);
+
+            //Act
+            var result = unixTimeConverter.ReadJson(jsonReaderMock.Object, null, false, null);
+
+            //Assert
+            Assert.Equal(new DateTime(2012,12,12,12,12,12, DateTimeKind.Utc), result);
+        }
+    }
+}
--- UnixTimeConverter/UnixTimeConverter/UnixTimeConverter.cs
+++ UnixTimeConverter/UnixTimeConverter/UnixTimeConverter.cs
@@ -11,7 +11,7 @@ namespace UnixTimeConverter
             if (!(value is DateTime))
                 throw new ArgumentException("Expect DateTime as input parameter");
 
-            var epoc = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            var epoc = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             var delta = ((DateTime) value).ToUniversalTime() - epoc;
 
             var ticks = (long) delta.TotalSeconds;
@@ -24,9 +24,9 @@ namespace UnixTimeConverter
             if (reader.TokenType != JsonToken.Integer && reader.TokenType != JsonToken.String)
                 throw new ArgumentException($"Unexpected token parsing date. Integer or String was expected, got {reader.TokenType}");
 
-            var ticks = (long) reader.Value;
+            var ticks = Convert.ToInt64(reader.Value);
 
-            var date = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            var date = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             date = date.AddSeconds(ticks);
 
             return date;
